77c562d099815b76bfe8d8961afda619ac217ad5
Robin Bertram <robin.bertram@gmail.com>
2021-09-21 09:22:11 +0200

    Optimization

--- deno/deno.ts
+++ deno/deno.ts
@@ -1,9 +1,9 @@
-const response = new TextEncoder().encode("Hello, World!");
+const response = new Response(new TextEncoder().encode("Hello, World!"));
 
 for await (const conn of Deno.listen({ port: 8000 })) {
     (async () => {
         for await (const { respondWith } of Deno.serveHttp(conn)) {
-            respondWith(new Response(response));
+            respondWith(response);
         }
     })();
 }
