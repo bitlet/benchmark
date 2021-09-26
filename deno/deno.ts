3f521cddb561baab2e7b758040684e08b5a0c97f
Robin Bertram <robin.bertram@gmail.com>
2021-09-26 12:05:41 +0200

    Update

--- deno/deno.ts
+++ deno/deno.ts
@@ -1,9 +1,9 @@
-const response = new Response(new TextEncoder().encode("Hello, World!"));
+const response = new TextEncoder().encode("Hello, World!");
 
 for await (const conn of Deno.listen({ port: 8000 })) {
     (async () => {
         for await (const { respondWith } of Deno.serveHttp(conn)) {
-            respondWith(response);
+            respondWith(new Response(response));
         }
     })();
 }
